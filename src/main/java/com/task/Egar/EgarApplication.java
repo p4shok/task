3579eeb53713096ad1b372726e03870dac3cc69a
p4shok <pavel_porshnev@icloud.com>
2024-06-04 19:12:21 +0300

    First commit

---
+++ src/main/java/com/task/Egar/EgarApplication.java
@@ -0,0 +1,13 @@
+package com.task.Egar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/task/Egar/EgarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.Egar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
